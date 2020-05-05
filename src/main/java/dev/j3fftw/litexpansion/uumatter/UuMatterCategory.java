3e53fc169e1960ae4bd6f1299502f84a4f04b265
Daniel Walsh <walshydev@gmail.com>
2020-05-05 20:53:18 +0100

    Add back buttons

--- src/main/java/dev/j3fftw/litexpansion/uumatter/UuMatterCategory.java
+++ src/main/java/dev/j3fftw/litexpansion/uumatter/UuMatterCategory.java
@@ -5,13 +5,12 @@ import dev.j3fftw.litexpansion.LiteXpansion;
 import io.github.thebusybiscuit.slimefun4.api.player.PlayerProfile;
 import io.github.thebusybiscuit.slimefun4.core.categories.FlexCategory;
 import io.github.thebusybiscuit.slimefun4.core.guide.SlimefunGuideLayout;
-import io.github.thebusybiscuit.slimefun4.core.guide.options.SlimefunGuideSettings;
 import io.github.thebusybiscuit.slimefun4.utils.ChestMenuUtils;
 import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ChestMenu;
-import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.ClickAction;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.SlimefunPlugin;
 import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
+import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -20,7 +19,7 @@ public final class UuMatterCategory extends FlexCategory {
 
     public static final UuMatterCategory INSTANCE = new UuMatterCategory();
 
-    private final int[] recipeSlots = new int[] {3, 4, 5, 12, 13, 14, 21, 22, 23};
+    private final int[] recipeSlots = new int[] {12, 13, 14, 21, 22, 23, 30, 31, 32};
     private ChestMenu menu;
 
     private UuMatterCategory() {
@@ -47,21 +46,35 @@ public final class UuMatterCategory extends FlexCategory {
     }
 
     private void displayItem(PlayerProfile profile, ItemStack result) {
-        Player p = profile.getPlayer();
+        final Player p = profile.getPlayer();
         if (p != null) {
-            ChestMenu menu = this.create(p);
+            final ChestMenu menu = this.create(p);
 
-//            RecipeChoiceTask task = new RecipeChoiceTask();
+            // Header and back button
+            for (int i = 0; i < 9; i++) {
+                if (i == 1) {
+                    menu.addItem(i, new CustomItem(ChestMenuUtils.getBackButton(p, "",
+                        ChatColor.GRAY + SlimefunPlugin.getLocal().getMessage(p, "guide.back.guide")))
+                    );
+                    menu.addMenuClickHandler(i, (pl, s, is, action) -> {
+                        open(p, profile, SlimefunGuideLayout.CHEST);
+                        return false;
+                    });
+                } else
+                    menu.addItem(i, ChestMenuUtils.getBackground(), ChestMenuUtils.getEmptyClickHandler());
+            }
 
             ItemStack[] recipe = UUMatter.INSTANCE.getRecipes().get(result);
             if (recipe == null) return;
 
             this.displayItem(menu, p, result, recipe);
 
+            // Panda wants a footer
+            for (int i = 36; i < 45; i++) {
+                menu.addItem(i, ChestMenuUtils.getBackground(), ChestMenuUtils.getEmptyClickHandler());
+            }
+
             menu.open(p);
-//            if (!task.isEmpty()) {
-//                task.start(menu.toInventory());
-//            }
         }
     }
 
@@ -72,8 +85,8 @@ public final class UuMatterCategory extends FlexCategory {
             menu.addItem(recipeSlots[i], recipe[i], clickHandler);
         }
 
-        menu.addItem(10, RecipeType.ENHANCED_CRAFTING_TABLE.getItem(p), ChestMenuUtils.getEmptyClickHandler());
-        menu.addItem(16, output, ChestMenuUtils.getEmptyClickHandler());
+        menu.addItem(19, RecipeType.ENHANCED_CRAFTING_TABLE.getItem(p), ChestMenuUtils.getEmptyClickHandler());
+        menu.addItem(25, output, ChestMenuUtils.getEmptyClickHandler());
     }
 
     @Override
@@ -83,9 +96,9 @@ public final class UuMatterCategory extends FlexCategory {
 
     @Override
     public void open(Player player, PlayerProfile playerProfile, SlimefunGuideLayout slimefunGuideLayout) {
-        menu.addItem(1, ChestMenuUtils.getMenuButton(player));
+        menu.addItem(1, ChestMenuUtils.getBackButton(player));
         menu.addMenuClickHandler(1, (pl, slot, item, action) -> {
-            SlimefunGuideSettings.openSettings(pl, pl.getInventory().getItemInMainHand());
+            SlimefunPlugin.getRegistry().getGuideLayout(slimefunGuideLayout).openMainMenu(playerProfile, 1);
             return false;
         });
 
