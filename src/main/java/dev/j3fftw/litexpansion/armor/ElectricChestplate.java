2673523cb58196375cf65ad0f9af3641fc61288f
Panda.com <J3fftw@gmail.com>
2020-09-09 12:21:57 +0200

    changed some more recipes

--- src/main/java/dev/j3fftw/litexpansion/armor/ElectricChestplate.java
+++ src/main/java/dev/j3fftw/litexpansion/armor/ElectricChestplate.java
@@ -1,6 +1,7 @@
 package dev.j3fftw.litexpansion.armor;
 
 import dev.j3fftw.litexpansion.Items;
+import dev.j3fftw.litexpansion.machine.MetalForge;
 import io.github.thebusybiscuit.slimefun4.core.attributes.Rechargeable;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
@@ -9,7 +10,7 @@ import org.bukkit.inventory.ItemStack;
 public class ElectricChestplate extends SlimefunItem implements Rechargeable {
 
     public ElectricChestplate() {
-        super(Items.LITEXPANSION, Items.ELECTRIC_CHESTPLATE, RecipeType.ENHANCED_CRAFTING_TABLE, new ItemStack[] {
+        super(Items.LITEXPANSION, Items.ELECTRIC_CHESTPLATE, MetalForge.METAL_FORGE, new ItemStack[] {
             Items.MAG_THOR, null, Items.MAG_THOR,
             Items.MAG_THOR, Items.MAG_THOR, Items.MAG_THOR,
             Items.MAG_THOR, Items.MAG_THOR, Items.MAG_THOR
