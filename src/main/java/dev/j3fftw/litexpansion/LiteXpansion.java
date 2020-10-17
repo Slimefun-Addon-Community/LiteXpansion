b8d9086a05d9368f65401a7deeb81fce526e56df
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-10-18 01:54:22 +0200

    Fix enchantments and bStats

--- src/main/java/dev/j3fftw/litexpansion/LiteXpansion.java
+++ src/main/java/dev/j3fftw/litexpansion/LiteXpansion.java
@@ -1,17 +1,14 @@
 package dev.j3fftw.litexpansion;
 
-import dev.j3fftw.litexpansion.armor.ElectricChestplate;
-import dev.j3fftw.litexpansion.items.Wrench;
-import dev.j3fftw.litexpansion.resources.ThoriumResource;
-import dev.j3fftw.litexpansion.utils.Constants;
-import dev.j3fftw.litexpansion.uumatter.UUMatter;
-import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
-import io.github.thebusybiscuit.slimefun4.core.researching.Research;
-import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
-import me.mrCookieSlime.Slimefun.api.BlockStorage;
-import me.mrCookieSlime.Slimefun.bstats.bukkit.Metrics;
-import me.mrCookieSlime.Slimefun.cscorelib2.config.Config;
-import me.mrCookieSlime.Slimefun.cscorelib2.updater.GitHubBuildsUpdater;
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+
+import javax.annotation.Nonnull;
+
+import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.NamespacedKey;
@@ -20,12 +17,16 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import javax.annotation.Nonnull;
-import java.io.File;
-import java.lang.reflect.Field;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Level;
+import dev.j3fftw.litexpansion.armor.ElectricChestplate;
+import dev.j3fftw.litexpansion.resources.ThoriumResource;
+import dev.j3fftw.litexpansion.utils.Constants;
+import dev.j3fftw.litexpansion.uumatter.UUMatter;
+import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
+import io.github.thebusybiscuit.slimefun4.core.researching.Research;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
+import me.mrCookieSlime.Slimefun.api.BlockStorage;
+import me.mrCookieSlime.Slimefun.cscorelib2.config.Config;
+import me.mrCookieSlime.Slimefun.cscorelib2.updater.GitHubBuildsUpdater;
 
 public class LiteXpansion extends JavaPlugin implements SlimefunAddon {
 
@@ -58,9 +59,7 @@ public class LiteXpansion extends JavaPlugin implements SlimefunAddon {
             getLogger().warning("Failed to register enchantment. Seems the 'acceptingNew' field changed monkaS");
         }
 
-        Enchantment.registerEnchantment(new GlowEnchant(Constants.GLOW_ENCHANT, new String[] {
-                "ADVANCED_CIRCUIT", "NANO_BLADE", "GLASS_CUTTER"
-        }));
+        registerEnchantments();
 
         ItemSetup.INSTANCE.init();
 
@@ -83,6 +82,17 @@ public class LiteXpansion extends JavaPlugin implements SlimefunAddon {
     @Override
     public void onDisable() {
         instance = null;
+    }
+    
+    private void registerEnchantments() {
+        Enchantment glowEnchantment = new GlowEnchant(Constants.GLOW_ENCHANT, new String[] {
+                "ADVANCED_CIRCUIT", "NANO_BLADE", "GLASS_CUTTER"
+        });
+        
+        // Prevent double-registration errors
+        if (Enchantment.getByKey(glowEnchantment.getKey()) == null) {
+            Enchantment.registerEnchantment(glowEnchantment);
+        }
     }
 
     private void setupResearches() {
@@ -182,7 +192,7 @@ public class LiteXpansion extends JavaPlugin implements SlimefunAddon {
                         final SlimefunItem item = SlimefunItem.getByID(entry.getValue().getString("id"));
                         if (item == null || !(item.getAddon() instanceof LiteXpansion)) continue;
 
-                        data.merge(item.getID(), 1, Integer::sum);
+                        data.merge(item.getId(), 1, Integer::sum);
                     }
                 }
             } catch (ReflectiveOperationException e) {
