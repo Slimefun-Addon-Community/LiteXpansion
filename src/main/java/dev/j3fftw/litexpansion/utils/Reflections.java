d5c8a2af27a4198ff93ab1dfabdb764f7f7d48a9
Daniel Walsh <walshydev@gmail.com>
2021-03-06 16:54:25 +0000

    Fix metrics spike

--- src/main/java/dev/j3fftw/litexpansion/utils/Reflections.java
+++ src/main/java/dev/j3fftw/litexpansion/utils/Reflections.java
@@ -1,8 +1,9 @@
 package dev.j3fftw.litexpansion.utils;
 
-import java.lang.reflect.Field;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public final class Reflections {
 
@@ -28,6 +29,29 @@ public final class Reflections {
             f.set(instance, newValue); // NOSONAR
         } catch (ReflectiveOperationException e) {
             Log.warn("Failed to change field {} to {} in {}", fieldName, newValue, instance.getClass().getSimpleName());
+        }
+    }
+
+    public static Object getField(@Nonnull Class<?> clazz, @Nullable Object instance, @Nonnull String fieldName) {
+        try {
+            final Field f = clazz.getDeclaredField(fieldName);
+            f.setAccessible(true); // NOSONAR
+
+            return f.get(instance);
+        } catch (ReflectiveOperationException e) {
+            Log.warn("Failed to get field {} in {}", fieldName, clazz.getSimpleName());
+            return null;
+        }
+    }
+
+    public static void invoke(@Nonnull Class<?> clazz, @Nullable Object instance, @Nonnull String methodName) {
+        try {
+            final Method m = clazz.getDeclaredMethod(methodName);
+            m.setAccessible(true); // NOSONAR
+
+            m.invoke(instance);
+        } catch (ReflectiveOperationException e) {
+            Log.warn("Failed to invoke method {} in {}", methodName, clazz.getSimpleName());
         }
     }
 }
