8ae819e2b7a2750eb388e5c03f9f5539f421aca0
md5sha256 <42793301+md5sha256@users.noreply.github.com>
2021-09-05 03:56:39 +0800

    Fix compile error

--- src/main/java/dev/j3fftw/litexpansion/Items.java
+++ src/main/java/dev/j3fftw/litexpansion/Items.java
@@ -26,7 +26,7 @@ public final class Items {
     public static final ItemGroup LITEXPANSION = new ItemGroup(
             new NamespacedKey(LiteXpansion.getInstance(), "litexpansion"),
             new CustomItemStack(PlayerHead.getItemStack(PlayerSkin.fromHashCode("3f87fc5cbb233743a82fb0fa51fe739487f29bcc01c9026621ecefad197f4fb1")),
-                    "&7LiteXpansion"));
+                "&7LiteXpansion"));
 
     public static final SlimefunItemStack ELECTRIC_CHESTPLATE = new SlimefunItemStack(
         "ELECTRIC_CHESTPLATE",
--- src/main/java/dev/j3fftw/litexpansion/machine/generators/Generator.java
+++ src/main/java/dev/j3fftw/litexpansion/machine/generators/Generator.java
@@ -1,9 +1,9 @@
 package dev.j3fftw.litexpansion.machine.generators;
 
 import dev.j3fftw.litexpansion.Items;
+import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.implementation.items.electric.generators.CoalGenerator;
 import javax.annotation.Nonnull;
-import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.abstractItems.MachineFuel;
 import org.bukkit.Material;
 import org.bukkit.Tag;
