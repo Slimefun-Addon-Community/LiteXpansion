a6bc14e7fd64fb057056b5265697cc5b0986e7be
Jeffrey Kosse <j3fftw@gmail.com>
2022-08-13 15:00:50 +0200

    removed import

--- src/main/java/dev/j3fftw/litexpansion/machine/UUCrafter.java
+++ src/main/java/dev/j3fftw/litexpansion/machine/UUCrafter.java
@@ -19,7 +19,6 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Entity;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
